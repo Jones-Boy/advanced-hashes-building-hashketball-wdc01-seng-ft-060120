801b9faf9b9f9ca7899cdeb2e76711d01ce00a7b
Michael Jones <djyoungjones@gmail.com>
2020-05-24 21:13:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,14 +115,5 @@ def game_hash
   }
 end 
 
-def num_points_scored(name)
-  hash = game_hash
-  hash.each do |location, info| 
-    info.each do |attribute, stuff| 
-      if stuff.include?(name) 
-       return hash[location][attribute][name][:points]
-      end
-    end
-  end
-end
+
 
